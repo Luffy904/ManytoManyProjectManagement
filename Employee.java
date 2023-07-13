a1895a70ee057220500c31b5a3384bed392e6f0a
Amit Bind <131703126+Luffy904@users.noreply.github.com>
2023-07-13 14:37:44 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,44 @@
+package com.example.ManytoMany;
+import java.util.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="WiproEmployee")
+public class Employee {
+	@Id
+private int eid;
+	private String name;
+	private String position;
+	@ManyToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="pid")
+	private List<Project> projects =new ArrayList<Project>();
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public List<Project> getProjects() {
+		return projects;
+	}
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+}
